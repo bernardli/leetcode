10f33902c0143abd3e4c788724de9e879b84236d
bernardli <bernardli1248@qq.com>
2019-09-19 09:05:24 +0800

    fix Q1 bug, add test

--- 1_two_sum/1.py
+++ 1_two_sum/1.py
@@ -1,10 +1,21 @@
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         record = {}
         numsLen = len(nums)
         for i in range(numsLen):
-            if i <= target:
-                if (target - nums[i]):
-                    return [record[target - nums[i]], i]
+            if nums[i] <= target:
+                if nums[i] in record:
+                    return [record[nums[i]], i]
                 else:
                     record[target - nums[i]] = i
+
+    def run(self):
+        print(self.twoSum([2, 11, 15, 7], 9))
+
+
+# test
+foo = Solution()
+foo.run()
